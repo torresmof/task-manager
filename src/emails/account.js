6ff000604e6d1226b45ceac2adb11cc1016fa7f0
Gabriel <gabrieloliveira12488@gmail.com>
2019-06-03 16:18:30 -0300

    first commit

--- src/emails/account.js
+++ src/emails/account.js
@@ -1,6 +1,6 @@
 const sgMail = require("@sendgrid/mail");
 
-const sendgridKey = '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';
+const sendgridKey = process.env.SENDGRIDKEY;
 sgMail.setApiKey(sendgridKey);
 
 sgMail.send({
